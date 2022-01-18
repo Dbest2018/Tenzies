3e6e71749c9807590e20513dbd73d9fb3f0dd9c1
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-18 12:05:21 +0000

    feat: Add functionality to Roll button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Dice from "./components/Dice";
 import styled from "styled-components";
+import { nanoid } from "nanoid";
+
+import Dice from "./components/Dice";
 
 function App() {
   const [newDice, setNewDice] = useState([]);
@@ -13,19 +15,55 @@ function App() {
     const randomDice = [];
     const maxNumOnDice = 6;
     for (let i = 0; i < 10; i++) {
-      randomDice.push(Math.ceil(Math.random() * maxNumOnDice));
+      randomDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * maxNumOnDice),
+        isHeld: false,
+      });
     }
     return randomDice;
   };
 
+  const rollDice = () => {
+    setNewDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld
+          ? die
+          : {
+              id: nanoid(),
+              value: Math.ceil(Math.random() * 6),
+              isHeld: false,
+            };
+      })
+    );
+  };
+
+  const holdDice = (id) => {
+    setNewDice((prevDice) => {
+      return prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      });
+    });
+  };
+
   return (
     <AppContainer>
+      <Title>Tenzies</Title>
+      <Instruction>
+        Roll until all dice are the same. Click each die to freeze it at it
+        current value between rolls
+      </Instruction>
       <DiceContainer>
-        {newDice.map((dice, index) => (
-          <Dice key={index} value={dice} />
+        {newDice.map((dice) => (
+          <Dice
+            key={dice.id}
+            value={dice.value}
+            isHeld={dice.isHeld}
+            holdDice={() => holdDice(dice.id)}
+          />
         ))}
       </DiceContainer>
-      <button onClick={() => setNewDice(allNewDice())}>Roll</button>
+      <button onClick={rollDice}>Roll</button>
     </AppContainer>
   );
 }
@@ -53,6 +91,26 @@ const AppContainer = styled.main`
     border-radius: 10px;
     cursor: pointer;
   }
+
+  > button:focus {
+    outline: none;
+  }
+
+  > button:active {
+    box-shadow: inset 5px 5px 10px -3px rgba(0, 0, 0, 0.7);
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 40px;
+  margin: 0;
+`;
+
+const Instruction = styled.p`
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+  margin-top: 0;
+  text-align: center;
 `;
 
 const DiceContainer = styled.div`
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Dice = ({ value }) => {
+const Dice = ({ id, value, isHeld, holdDice }) => {
+  const styles = {
+    backgroundColor: isHeld ? "#59e391" : "white",
+  };
   return (
-    <DieFace>
+    <DieFace style={styles} onClick={holdDice}>
       <h2>{value}</h2>
     </DieFace>
   );
@@ -19,6 +22,17 @@ const DieFace = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: white;
   cursor: pointer;
 `;
+
+// const DieFaceGreen = styled.div`
+//   height: 50px;
+//   width: 50px;
+//   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
+//   border-radius: 10px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: #59e391;
+//   cursor: pointer;
+// `;
